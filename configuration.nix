91ff519eae6dc8d8de747a1e95fd888970191e93
Marc Partensky <marc@marcpartensky.com>
2023-09-15 19:19:22 +0200

    gdm

--- configuration.nix
+++ configuration.nix
@@ -70,13 +70,13 @@ in
       #   enable = true; 
       #   user = "marc"; 
       # }; 
-      lightdm = { 
-        enable = true; 
-        greeter.enable = true; 
-      }; 
-      # gdm = {
-      #   enable = true;
-      # };
+      # lightdm = { 
+      #   enable = true; 
+      #   greeter.enable = true; 
+      # }; 
+      gdm = {
+        enable = true;
+      };
     };
   };
 
