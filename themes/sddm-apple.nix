b74b8e12b42d368a473674ebc9537802fb1c6e5d
Marc Partensky <marc@marcpartensky.com>
2023-09-15 20:33:37 +0200

    apple

--- themes/sddm-apple.nix
+++ themes/sddm-apple.nix
@@ -32,7 +32,7 @@ rec {
 
   installPhase = ''
     mkdir -p $out/share/sddm/themes
-    cp -aR $src $out/share/sddm/themes/sddm-peace-color
+    cp -aR $src $out/share/sddm/themes/sddm-aple
   '';
 
 }
