b1bd9734576ef7fe78a3f6b88dc4df5f95724853
installer <installer@gmail.com>
2025-06-18 04:46:07 +0200

    hyprland

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -1,15 +1,25 @@
-{ inputs, pkgs, lib, config, ... }:
+# { inputs, pkgs, lib, config, ... }:
 
-with lib;
-let cfg = config.modules.hyprland;
+# with lib;
+# let cfg = config.modules.hyprland;
 
-in {
-    options.modules.hyprland= { enable = mkEnableOption "hyprland"; };
-    config = mkIf cfg.enable {
-	home.packages = with pkgs; [
-	    wofi swaybg wlsunset wl-clipboard hyprland
-	];
+# in {
+#     options.modules.hyprland= { enable = mkEnableOption "hyprland"; };
+#     config = mkIf cfg.enable {
+# 	home.packages = with pkgs; [
+# 	    wofi swaybg wlsunset wl-clipboard hyprland
+# 	];
 
-        home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
-    };
+#         home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
+#     };
+# }
+
+{inputs, pkgs, ...}: {
+  programs.hyprland = {
+    enable = true;
+    # set the flake package
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # make sure to also set the portal package, so that they are in sync
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
 }
