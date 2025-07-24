0e484b249d9a70e9ade8cf14bd95ee999f045435
marc partensky <marc@marcpartensky.com>
2025-07-24 16:03:38 +0200

    iwd backend for network manager

--- hosts/laptop/networking.nix
+++ hosts/laptop/networking.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -11,14 +11,29 @@
   networking = {
     useDHCP = lib.mkDefault true;
     hostName = "nixos";
-    networkmanager.enable = true; # enable network manager to start during boot
     nameservers = [ "8.8.8.8" "8.8.4.4" ];
     enableIPv6 = false;
+
     # nat = {
     #   enable = true;
     #   internalInterfaces = ["ve-+"];
     #   externalInterface = "enp0s31f6"; # eth interface
     # };
+
+    networkmanager.enable = true; # enable network manager to start during boot
+    networkmanager.wifi.backend = "iwd";
+
+    wireless.iwd = {
+      enable = true;
+      settings = {
+        IPv6 = {
+          Enabled = true;
+        };
+        Settings = {
+          AutoConnect = true;
+        };
+      };
+    };
   };
 
 }
