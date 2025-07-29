ac3fb69dafed1896822231b5529ef87c31a422ff
marc partensky <marc@marcpartensky.com>
2025-07-29 19:02:54 +0200

    kodi wayland + plugins

--- modules/kodi/default.nix
+++ modules/kodi/default.nix
@@ -1,24 +1,12 @@
 { pkgs, ... }: {
-
-  # users.extraUsers.kodi.isNormalUser = true;
-  # services.cage.user = "kodi";
-  # services.cage.program = "${pkgs.kodi-wayland}/bin/kodi-standalone";
-  # services.cage.enable = true;
-
-  environment.systemPackages = [
-	(pkgs.kodi.withPackages (kodiPkgs: with kodiPkgs; [
-		jellyfin
-	]))
-  ];
-
-
-  # Define a user account
-  home-manager.users.marc = { pkgs, inputs, ... }: {
-
-    home.packages = with pkgs; [
-   #    (kodi.withPackages (kodiPkgs: with kodiPkgs; [
-	  # 	jellyfin
-	  # ]))
+  home-manager.users.marc = { pkgs, ... }: {
+    home.packages = [
+      (pkgs.kodi-wayland.withPackages (kodiPkgs: with kodiPkgs; [
+        jellyfin
+        jellycon
+        netflix
+        youtube
+      ]))
     ];
   };
 }
