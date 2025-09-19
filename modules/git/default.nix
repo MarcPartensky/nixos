8f791f3e537c58bebb9b2ec2d28a5f23031a939b
marc <marc@marcpartensky.com>
2025-09-19 17:13:36 +0200

    weak git conf

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -1,11 +1,16 @@
 { pkgs, ... }:
 {
-  programs.git = {
-    enable = true;
-    # aliases = {
-    #   ci = "commit";
-    #   co = "checkout";
-    #   s = "status";
-    # };
+
+  home-manager.users.marc = { pkgs, inputs, ... }: {
+    programs.git = {
+      userName = "marc";       # replace with your name
+      userEmail = "marc@marcpartensky.com"; # replace with your email
+      enable = true;
+      # aliases = {
+      #   ci = "commit";
+      #   co = "checkout";
+      #   s = "status";
+      # };
+    };
   };
 }
