6c348e93d4f4b3ecb474c934eb804dd4ed606609
marc <marc@marcpartensky.com>
2025-11-03 17:00:45 +0700

    postgres

--- services/postgres/default.nix
+++ services/postgres/default.nix
@@ -5,7 +5,14 @@
     authentication = pkgs.lib.mkOverride 10 ''
       #type database  DBuser  auth-method
       local all       all     trust
+      
+      # IPv4 localhost
+      host all all 127.0.0.1/32 trust
+
+      # IPv6 localhost
+      host all all ::1/128 trust
     '';
+    ensureDatabases = [ "vaultwarden" ];
       # Script SQL initial (doit être un fichier réel)
     initialScript = pkgs.writeText "vaultwarden-init.sql" ''
       CREATE USER vaultwarden WITH LOGIN PASSWORD 'vaultwarden';
