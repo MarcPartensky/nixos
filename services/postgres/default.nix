7e7ef123397c5ea4afbdc913c6e5eb40739bda51
Marc Partensky <marc.partensky@gmail.com>
2025-11-25 16:56:38 +0100

    postgres

--- services/postgres/default.nix
+++ services/postgres/default.nix
@@ -1,24 +1,36 @@
-{ pkgs, ... }: {
-  config.services.postgresql = {
+{ pkgs, ... }:
+
+{
+  services.postgresql = {
     enable = true;
+
+    # Crée les bases automatiquement
     ensureDatabases = [ "nextcloud" "vaultwarden" ];
-    ensureUsers = [ "nextcloud" "vaultwarden" ];
+
+    # Authentification (locale et localhost)
     authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
-      local all       all     trust
-      
+      # type database  DBuser  auth-method
+      local all all trust
+
       # IPv4 localhost
       host all all 127.0.0.1/32 trust
 
       # IPv6 localhost
       host all all ::1/128 trust
     '';
-      # Script SQL initial (doit être un fichier réel)
-    initialScript = pkgs.writeText "vaultwarden-init.sql" ''
+
+    # Script SQL initial pour créer utilisateurs et droits
+    initialScript = pkgs.writeText "init.sql" ''
+      CREATE USER root WITH LOGIN PASSWORD 'rootpassword' SUPERUSER;
+
+      -- Nextcloud
+      CREATE USER nextcloud WITH LOGIN PASSWORD 'nextcloudpassword';
+      GRANT ALL PRIVILEGES ON DATABASE nextcloud TO nextcloud;
+
+      -- Vaultwarden
       CREATE USER vaultwarden WITH LOGIN PASSWORD 'vaultwarden';
-      CREATE USER root WITH LOGIN PASSWORD 'rootpassword';
       GRANT ALL PRIVILEGES ON DATABASE vaultwarden TO vaultwarden;
-      GRANT ALL PRIVILEGES ON DATABASE root TO vaultwarden;
     '';
   };
 }
+
