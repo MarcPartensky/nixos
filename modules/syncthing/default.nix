d41a0a4f2c5c796ee6df48668f6e76a0872262e9
marc <marc@marcpartensky.com>
2025-12-08 00:31:55 +0100

    syncthing precise

--- modules/syncthing/default.nix
+++ modules/syncthing/default.nix
@@ -8,12 +8,18 @@
       # dataDir = "/home/marc";
       # configDir = "/home/marc/syncthing";
       settings = {
+        option.urAccepted = 1;
         devices = {
-          xiaomi12.id = "PEAKD4W-MDXJTZQ-C5WZWXZ-AW72EK3-WLFLM2L-A2GVBEM-HOB62UJ-BNHUQAB";
+          "xiaomi12" ={
+	    id = "PEAKD4W-MDXJTZQ-C5WZWXZ-AW72EK3-WLFLM2L-A2GVBEM-HOB62UJ-BNHUQAB";
+	    autoAcceptFolders = true;
+	  };
         };
       	folders."marc" = {
 	  path = "/home/marc/synthing";
 	  devices = [ "xiaomi12" ];
+	  type = "sendreceive";
+	  # one of "sendreceive", "sendonly", "receiveonly", "receiveencrypted"
 	};
       };
       guiAddress = "127.0.0.1:8384";
