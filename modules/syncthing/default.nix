a37553d40590078c426ff5e6e36e02d9d0a8ee44
marc <marc@marcpartensky.com>
2025-12-09 04:51:51 +0100

    fix indent

--- modules/syncthing/default.nix
+++ modules/syncthing/default.nix
@@ -11,16 +11,21 @@
         option.urAccepted = 1;
         devices = {
           "xiaomi12" ={
-	    id = "PEAKD4W-MDXJTZQ-C5WZWXZ-AW72EK3-WLFLM2L-A2GVBEM-HOB62UJ-BNHUQAB";
-	    autoAcceptFolders = true;
-	  };
+      	    id = "PEAKD4W-MDXJTZQ-C5WZWXZ-AW72EK3-WLFLM2L-A2GVBEM-HOB62UJ-BNHUQAB";
+      	    autoAcceptFolders = true;
+      	  };
         };
-      	folders."marc" = {
-	  path = "/home/marc/sync";
-	  devices = [ "xiaomi12" ];
-	  type = "sendreceive";
-	  # one of "sendreceive", "sendonly", "receiveonly", "receiveencrypted"
-	};
+      	folders."Camera" = {
+      	  path = "/home/marc/sync/Camera";
+      	  devices = [ "xiaomi12" ];
+      	  type = "sendreceive";
+      	  # one of "sendreceive", "sendonly", "receiveonly", "receiveencrypted"
+      	};
+      	folders."Personal" = {
+      	  path = "/home/marc/sync/Personal";
+      	  devices = [ "xiaomi12" ];
+      	  type = "sendreceive";
+      	};
       };
       guiAddress = "127.0.0.1:8384";
       # openDefaultPorts = true;
