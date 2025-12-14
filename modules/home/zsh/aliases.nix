5ebbea34d30c5bf2945771c31d8fad7f57e70edf
marc <marc@marcpartensky.com>
2025-12-14 04:39:50 +0100

    aliases

--- modules/home/zsh/aliases.nix
+++ modules/home/zsh/aliases.nix
@@ -11,8 +11,18 @@
   r = "restart";
   g = "grep";
 
-  sy = "systemctl";
   ll = "ls -l";
+  wh = "which";
+  dc = "docker compose";
+  ipi = "http ipinfo.io";
+
+  sy = "systemctl";
+  sys = "systemctl status";
+  syr = "systemctl restart";
+  syus = "systemctl --user status";
+  syur = "systemctl --user restart";
+
+
   gs = "git status";
   ga = "git add -A";
   gc = "git commit -m";
@@ -24,12 +34,7 @@
   gpl = "git pull";
   grsh = "git reset --hard HEAD";
   grs = "git reset";
-  wh = "which";
-  sshp = "cat ~/.ssh/id_ed25519.pub";
-  update = "sudo nixos-rebuild switch /etc/nixos#laptop";
-  ipi = "http ipinfo.io";
-  dc = "docker compose";
-  # ns = "nix-search-tv print |
-  #   fzf --preview 'nix-search-tv preview {}' --scheme history";
 
+  sshp = "cat ~/.ssh/id_ed25519.pub";
+  update = "just -f $HOME/git/nixos/Justfile;
 }
