6aa18790a700853bda8c906b525a142de0491cf6
marc <marc@marcpartensky.com>
2025-12-14 02:54:59 +0100

    nextcloud 8081

--- services/nextcloud/default.nix
+++ services/nextcloud/default.nix
@@ -18,8 +18,9 @@ in {
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud32;
-    hostName = "localhost";
+    hostName = "localhost:8081";
     autoUpdateApps.enable = true;
+    appstoreEnable = true;
     config = {
         dbtype = "pgsql";
         dbuser = "nextcloud";
