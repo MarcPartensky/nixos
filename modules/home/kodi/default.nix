20be24b83e94654ecce3b27d78ecd68b279550d8
marc <marc@marcpartensky.com>
2025-12-16 05:00:57 +0100

    users

--- modules/home/kodi/default.nix
+++ modules/home/kodi/default.nix
@@ -1,12 +1,28 @@
-{ pkgs, ... }: {
-  home-manager.users.marc = { pkgs, ... }: {
-    home.packages = [
-      (pkgs.kodi-wayland.withPackages (kodiPkgs: with kodiPkgs; [
-        jellyfin
-        jellycon
-        netflix
-        youtube
-      ]))
+{ pkgs, ... }:
+let
+  homeDir = "/home/marc";
+in {
+  programs.kodi = {
+    enable = true;
+    package = pkgs.kodi-wayland.withPackages (kodiPkgs: with kodiPkgs; [
+      jellyfin
+      jellycon
+      netflix
+      youtube
+    ]);
+    # settings = {
+    # };
+    # La clé sources.video correspond à la section <video> dans sources.xml
+    sources.video = [
+      {
+        name = "movies";
+        path = "${homeDir}/media/movies/";
+        # 'allowsharing = true;' partage upnp
+      }
+      {
+        name = "tvshows";
+        path = "${homeDir}/media/tvshows/";
+      }
     ];
   };
 }
