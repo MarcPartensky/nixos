c133d4442d84e0d48ebff91ea6db3991e6aa30f5
marc <marc@marcpartensky.com>
2026-01-02 17:51:30 +0100

    hyprland flakes are no longer necessary with function niri

--- flake.nix
+++ flake.nix
@@ -5,10 +5,10 @@
     # darwin.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    hyprland.url = "github:hyprwm/Hyprland?ref=v0.50.0";
-    hy3.url = "github:outfoxxed/hy3?ref=hl0.50.0";
+    # hyprland.url = "github:hyprwm/Hyprland?ref=v0.50.0";
+    # hy3.url = "github:outfoxxed/hy3?ref=hl0.50.0";
     # hy3.url = "github:outfoxxed/hy3";
-    hy3.inputs.hyprland.follows = "hyprland";
+    # hy3.inputs.hyprland.follows = "hyprland";
     # nix-search-tv.url = "github:3timeslazy/nix-search-tv";
     catppuccin.url = "github:catppuccin/nix";
     newt.url = "github:fosrl/newt";
@@ -33,10 +33,10 @@
     #   url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
-    hyprtasking = {
-      url = "github:raybbian/hyprtasking";
-      inputs.hyprland.follows = "hyprland";
-    };
+    # hyprtasking = {
+    #   url = "github:raybbian/hyprtasking";
+    #   inputs.hyprland.follows = "hyprland";
+    # };
     catppuccin-thunderbird = {
       url = "github:catppuccin/thunderbird";
       flake = false; # The repo isn't a flake; use as raw source
@@ -47,10 +47,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
+    # hyprland-plugins = {
+    #   url = "github:hyprwm/hyprland-plugins";
+    #   inputs.hyprland.follows = "hyprland";
+    # };
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -66,7 +66,7 @@
     agenix.url = "github:ryantm/agenix";
     microvm.url = "github:astro/microvm.nix";
 
-    nwg-dock-hyprland-pin-nixpkgs.url = "nixpkgs/2098d845d76f8a21ae4fe12ed7c7df49098d3f15";
+    # nwg-dock-hyprland-pin-nixpkgs.url = "nixpkgs/2098d845d76f8a21ae4fe12ed7c7df49098d3f15";
 
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
@@ -82,7 +82,7 @@
         specialArgs = { inherit inputs; };
         modules = [
           inputs.disko.nixosModules.disko
-	      inputs.hyprland.nixosModules.default
+          # inputs.hyprland.nixosModules.default
 	      inputs.nixvim.nixosModules.default
           inputs.catppuccin.nixosModules.catppuccin
           # inputs.sops.nixosModules.sops
@@ -150,8 +150,6 @@
         };
         modules = [
           # inputs.disko.homeModule.disko
-          # inputs.home-manager.homeModule.default
-          # inputs.hyprland.homeManagerModules.default
       	  inputs.catppuccin.homeModules.catppuccin
           inputs.nixvim.homeModules.default
           inputs.sops.homeManagerModules.sops
