fde36e8571ef95c6a6a227c789c3854ec79fd2df
marc <marc@marcpartensky.com>
2026-01-03 23:14:47 +0100

    tor browser in container but seems too annoying to use

---
+++ modules/home/tor-browser/container.nix
@@ -0,0 +1,42 @@
+{ pkgs, ... } : {
+  containers.browser = {
+    autoStart = false;
+    privateNetwork = true;
+    hostAddress = "192.168.7.10";
+    localAddress = "192.168.7.11";
+    config = {config, pkgs, ... }: {
+      services.openssh = {
+        enable = true;
+        forwardX11 = true;
+      };
+  
+      users.extraUsers.browser = {
+        isNormalUser = true;
+        home = "/home/browser";
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINye6ye9NEWOZC2UboNAChnVGTTKuMieQPvuckskvXno" ];
+        extraGroups = ["audio" "video"];
+      };
+    };
+  };
+  
+  # Open necessary ports
+  networking.firewall.allowedTCPPorts = [ 4713 6000 ];
+  hardware.pulseaudio = {
+    # enable = true;
+    # systemWide = true;
+    # support32Bit = true;
+    tcp = { enable = true; anonymousClients = { allowedIpRanges = ["127.0.0.1" "192.168.7.0/24"]; }; };
+  };
+  
+  # Configuring NAT
+  networking.nat.enable = true;
+  networking.nat.internalInterfaces = ["ve-browser"];
+  networking.nat.externalInterface = "YOUR-EXTERNAL-INTERFACE";
+  
+  # Depending on your use of global or home configuration, you will have to install "socat"
+  environment.systemPackages = [
+    pkgs.socat
+  ];
+  
+  
+}
