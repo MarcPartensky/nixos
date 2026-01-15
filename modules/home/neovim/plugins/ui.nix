5e3885c1a0ad48e8963ad8fabbf5c33641807406
marc <marc@marcpartensky.com>
2026-01-15 18:32:36 +0100

    git status warning fix

--- modules/home/neovim/plugins/ui.nix
+++ modules/home/neovim/plugins/ui.nix
@@ -10,7 +10,7 @@
 
       neo-tree = {
         enable = true;
-        enableGitStatus = true;
+        settings.enable_git_status = true;
         # Si tu as un fichier de settings séparé :
         # settings = import ./neo-tree-settings.nix;
       };
