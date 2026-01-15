0bf4adcf6051bf97121457cc0825603f5034b9f7
marc <marc@marcpartensky.com>
2026-01-15 18:32:46 +0100

    feat(gammastep): add day and night brightness settings
    Configure screen brightness for day and night.

--- modules/home/gammastep/default.nix
+++ modules/home/gammastep/default.nix
@@ -4,5 +4,12 @@
     provider = "manual";
     latitude = 48.0;
     longitude = 2.0;
+    settings = {
+      general = {
+        # Definit la luminosite
+        brightness-day = 1.0;
+        brightness-night = 0.4;
+      };
+    };
   };
 }
