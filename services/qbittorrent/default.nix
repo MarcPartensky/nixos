082a6b9c284918c39f5087672f3132bffc9d6ab8
marc <marc@marcpartensky.com>
2026-01-17 01:33:25 +0100

    new password

--- services/qbittorrent/default.nix
+++ services/qbittorrent/default.nix
@@ -4,14 +4,14 @@
   services.qbittorrent = {
     enable = true;
     webuiPort = 8084;
-    torrentingPort = 8085;
+    # torrentingPort = 8085;
     serverConfig = {
       LegalNotice.Accepted = true; # Souvent nécessaire pour le démarrage
       Preferences = {
         WebUI = {
           Username = "marc"; # Remplacez par le nom d'utilisateur souhaité
           Password_PBKDF2 =
-          "@ByteArray(SsGjkRSKiMK3xulanOsYwA==:EgV/gBmgvnEm9uuudKAmYRWLFuPhq4qUn21vx6Hf5kKayPiXrIYrPxeO7dx2h7Aj/Id7cnvilhMt05GzaB/dfQ==)"; # Remplacez par le hash de votre mot de passe
+            "@ByteArray(wUQ/AMQATShMtwm8UpQfGQ==:8bUAN8WTM5IEQ3qzDjYkiA4Rj/23RxnVfZLHk0M1REMyfH7vKLY7RwFMmgQf9lyNkr/KOlRvK9MN1s5uKGC7Dg==)";
           # LocalHostAuth = true; # Optionnel: Peut être utile pour l'accès local
         };
       };
