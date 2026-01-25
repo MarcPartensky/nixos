2af9deba0c28046f33ff3d8d17f4a985af53a603
marc <marc@marcpartensky.com>
2026-01-25 01:08:45 +0100

    feat(zathura): add zathura program configuration
    Enable zathura and configure it with dark mode defaults and clipboard selection.

---
+++ modules/home/zathura/default.nix
@@ -0,0 +1,16 @@
+{ ... }: {
+  programs.zathura = {
+    enable = true;
+    options = {
+      # recolor = true; # dark background default
+      recolor-keephue = false;
+      
+      default-bg = "#000000";
+      default-fg = "#ffffff";
+      
+      recolor-lightcolor = "#000000";
+      recolor-darkcolor = "#ffffff";
+      selection-clipboard = "clipboard";
+    };
+  };
+}
