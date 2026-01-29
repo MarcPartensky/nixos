c7488fe00f22cedeb6cae71c856c947c5f828bc3
marc <marc@marcpartensky.com>
2026-01-29 02:37:43 +0100

    chore(zathura): disable explicit color options
    Comment out recolor and default color settings.

--- modules/home/zathura/default.nix
+++ modules/home/zathura/default.nix
@@ -3,13 +3,13 @@
     enable = true;
     options = {
       # recolor = true; # dark background default
-      recolor-keephue = false;
+      # recolor-keephue = false;
       
-      default-bg = "#000000";
-      default-fg = "#ffffff";
+      # default-bg = "#000000";
+      # default-fg = "#ffffff";
       
-      recolor-lightcolor = "#000000";
-      recolor-darkcolor = "#ffffff";
+      # recolor-lightcolor = "#000000";
+      # recolor-darkcolor = "#ffffff";
       selection-clipboard = "clipboard";
     };
   };
