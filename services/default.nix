29de740d0e8a46fe034ffbcf3b8db74d3c31a13f
marc <marc@marcpartensky.com>
2026-02-18 17:38:26 +0100

    feat(services): add flaresolverr import

--- services/default.nix
+++ services/default.nix
@@ -1,20 +1,24 @@
-{ pkgs, config, ... } :
 {
-    imports = [
-        ./nextcloud
-        ./postgres
-        ./vaultwarden
-        ./newt
-        ./readarr
-        ./radarr
-        ./sonarr
-        ./prowlarr
-        ./qbittorrent
-        # ./dnscrypt
-        ./adguard
-        ./navidrome
-        ./tor
-        # ./syncserver
-        # ./newt # attendre maj flakes
-    ];
+  pkgs,
+  config,
+  ...
+}: {
+  imports = [
+    ./nextcloud
+    ./postgres
+    ./vaultwarden
+    ./newt
+    ./readarr
+    ./radarr
+    ./sonarr
+    ./prowlarr
+    ./flaresolverr
+    ./qbittorrent
+    # ./dnscrypt
+    ./adguard
+    ./navidrome
+    ./tor
+    # ./syncserver
+    # ./newt # attendre maj flakes
+  ];
 }
