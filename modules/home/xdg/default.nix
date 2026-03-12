b6923ff7fc26911b16ccc94bdb233d3eb04b859f
marc <marc@marcpartensky.com>
2026-03-12 19:22:45 +0100

    feat(xdg): add desktop entry for Interactive Brokers TWS

--- modules/home/xdg/default.nix
+++ modules/home/xdg/default.nix
@@ -1,11 +1,21 @@
-{ ...} : {
+{...}: {
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "application/pdf" = [ "org.pwmt.zathura.desktop" ];
+      "application/pdf" = ["org.pwmt.zathura.desktop"];
       # "application/pdf" = [ "firefox.desktop" ];
     };
   };
+
+  xdg.desktopEntries.ib-tws = {
+    name = "Interactive Brokers TWS";
+    genericName = "Trading Workstation";
+    exec = "steam-run /home/marc/Jts/tws";
+    terminal = false;
+    categories = ["Application" "Network" "Finance"];
+    icon = "utilities-terminal"; # Pareil, à remplacer si tu as une belle icône
+  };
+
   # xdg.portal = {
   #   enable = true;
   #   config.common.default = "*";
