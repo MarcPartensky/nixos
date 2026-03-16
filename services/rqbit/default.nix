dcb03c97c18e2d6e363c191512113fd10f3d48ef
marc <marc@marcpartensky.com>
2026-03-16 22:05:33 +0100

    feat(rqbit): add user option and systemd config
    Allows specifying the user for the rqbit service.
    Includes commented-out systemd `RestrictAddressFamilies` for future use.

--- services/rqbit/default.nix
+++ services/rqbit/default.nix
@@ -1,7 +1,14 @@
-{...}: {
+{lib, ...}: {
+  # systemd.services.rqbit.serviceConfig.RestrictAddressFamilies = lib.mkForce [
+  #   "AF_INET"
+  #   "AF_INET6"
+  #   "AF_UNIX"
+  #   "AF_NETLINK"
+  # ];
   services.rqbit = {
     enable = true;
     httpPort = 3030;
     downloadDir = "/home/marc/downloads/";
+    user = "marc";
   };
 }
