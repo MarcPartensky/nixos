88d1efca90989ad4dd3270c8b694eb7a5758e2c6
marc <marc@marcpartensky.com>
2026-04-16 01:22:41 +0200

    feat(tower): add disko configuration
    Configures `/dev/nvme0n1` with GPT, ESP, encrypted swap, and a ZFS pool.
    Sets up ZFS datasets for root (encrypted), nix, and user home directories.

---
+++ hosts/tower/disko.nix
@@ -0,0 +1,114 @@
+{
+  disko.devices = {
+    disk = {
+      root = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["nofail"];
+              };
+            };
+            swap = {
+              # name = "swap";
+              # type = "8200";       # Linux swap
+              size = "16G";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+                priority = 100;
+              };
+            };
+            zfs = {
+              size = "1024G";
+              content = {
+                type = "zfs";
+                pool = "nixos";
+              };
+            };
+          };
+        };
+      };
+    };
+    zpool = {
+      nixos = {
+        type = "zpool";
+        rootFsOptions = {
+          mountpoint = "none";
+          compression = "lz4";
+          acltype = "posixacl";
+          xattr = "sa";
+          "com.sun:auto-snapshot" = "true";
+        };
+        options.ashift = "12";
+        datasets = {
+          "root" = {
+            type = "zfs_fs";
+            options = {
+              encryption = "aes-256-gcm";
+              keyformat = "passphrase";
+              #keylocation = "file:///tmp/secret.key";
+              keylocation = "prompt";
+            };
+            mountpoint = "/";
+          };
+          "nix" = {
+            type = "zfs_fs";
+            options.mountpoint = "/nix";
+            mountpoint = "/nix";
+          };
+          "marc" = {
+            type = "zfs_fs";
+            options.mountpoint = "none";
+          };
+          "marc/data" = {
+            type = "zfs_fs";
+            options.mountpoint = "/home/marc";
+          };
+          "marc/git" = {
+            type = "zfs_fs";
+            options.mountpoint = "/home/marc/git";
+          };
+          "marc/media" = {
+            type = "zfs_fs";
+            options.mountpoint = "/home/marc/media";
+          };
+          "marc/downloads" = {
+            type = "zfs_fs";
+            options.mountpoint = "/home/marc/downloads";
+          };
+          "marc/syncthing" = {
+            type = "zfs_fs";
+            options.mountpoint = "/home/marc/sync";
+          };
+
+          # # README MORE: https://wiki.archlinux.org/title/ZFS#Swap_volume
+          # "root/swap" = {
+          #   type = "zfs_volume";
+          #   size = "16G";
+          #   content = {
+          #     type = "swap";
+          #   };
+          #   options = {
+          #     volblocksize = "4096";
+          #     compression = "zle";
+          #     logbias = "throughput";
+          #     sync = "always";
+          #     primarycache = "metadata";
+          #     secondarycache = "none";
+          #     "com.sun:auto-snapshot" = "false";
+          #   };
+          # };
+        };
+      };
+    };
+  };
+}
