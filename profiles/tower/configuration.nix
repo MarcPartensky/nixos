9fde2bc9a5f9bcce17ede8b910e3d7666f691307
marc <marc@marcpartensky.com>
2026-04-22 03:07:34 +0200

    feat: enable firewall with allowed tcp ports for tower

--- profiles/tower/configuration.nix
+++ profiles/tower/configuration.nix
@@ -9,5 +9,14 @@
     ../../hosts/laptop/hardware-configuration.nix
   ];
 
+  networking.firewall = {
+    enable = true;
+
+    allowedTCPPorts = [
+      8083 # Pangolin / Apps
+      5432 # PostgreSQL
+    ];
+  };
+
   # sops.defaultSopsFile = ../../secrets/tower.yml;
 }
