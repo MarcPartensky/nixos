9c5bfdbe3f48d81a957624af2fcee70074691db0
marc <marc@marcpartensky.com>
2026-04-23 01:22:22 +0200

    zfs is too heavy

--- hosts/anywhere/boot.nix
+++ hosts/anywhere/boot.nix
@@ -15,7 +15,9 @@
   # Générer un hostId aléatoire ou fixe
   networking.hostId = "a1234e5e"; # 8 caractères hexadécimaux
 
-  boot.supportedFilesystems = [ "zfs" "ext4" ];
-  boot.initrd.supportedFilesystems = [ "zfs" "ext4" ];
+  # boot.supportedFilesystems = [ "zfs" "ext4" ];
+  # boot.initrd.supportedFilesystems = [ "zfs" "ext4" ];
+  boot.supportedFilesystems = [ "ext4" ];
+  boot.initrd.supportedFilesystems = [ "ext4" ];
   # boot.supportedFilesystems = [ "ext4" ];
 }
