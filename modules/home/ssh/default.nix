4da85889a446eac85e9d8f17e2b3683c3f3151ac
Marc Partensky <marc.partensky@gmail.com>
2026-05-17 10:08:19 +0200

    refactor: deduplicate ssh config with wildcard match block

--- modules/home/ssh/default.nix
+++ modules/home/ssh/default.nix
@@ -2,48 +2,32 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-
-    # extraConfig = "
-    #   Host rack
-    #     Hostname marcpartensky.com
-    #     Port 42069
-    #     User marc
-    # ";
     matchBlocks = {
-      "rack" = {
-        # host = "rack";
-        hostname = "marcpartensky.com";
-        user = "root";
-        port = 22;
+      "*" = {
         identityFile = "~/.ssh/id_ed25519";
         serverAliveInterval = 60;
         forwardAgent = true;
       };
-
+      "rack" = {
+        hostname = "marcpartensky.com";
+        user = "root";
+        port = 22;
+      };
       "towerlocal" = {
         hostname = "192.168.1.2";
         user = "marc";
         port = 22;
-        identityFile = "~/.ssh/id_ed25519";
-        serverAliveInterval = 60;
-        forwardAgent = true;
       };
       "tower" = {
         hostname = "77.207.176.170";
         user = "marc";
         port = 42070;
-        identityFile = "~/.ssh/id_ed25519";
-        serverAliveInterval = 60;
-        forwardAgent = true;
       };
       "tunnel" = {
-        hostname = "marpartensky.com";
+        hostname = "localhost";
         user = "marc";
-        port = 42070;
-        # proxyjump = "localhost";
-        identityFile = "~/.ssh/id_ed25519";
-        serverAliveInterval = 60;
-        forwardAgent = true;
+        port = 2222;
+        proxyJump = "rack";
       };
     };
   };
