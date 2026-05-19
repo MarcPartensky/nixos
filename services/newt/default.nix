44e1ac6c7ab116d15045b5212a83492de502390a
marc <marc@marcpartensky.com>
2026-05-20 00:12:56 +0200

    refactor: clean up newt service config and add sops mode

--- services/newt/default.nix
+++ services/newt/default.nix
@@ -1,16 +1,12 @@
-# services/newt/default.nix
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   sops.secrets.newt_env = {
     sopsFile = ../../secrets/common.yml;
-    # format = "yaml";
-    # key = "newt_env";
-
-    # sops extrait la valeur de la clef newt_env et l'écrit dans un fichier
-    # ce fichier sera au format dotenv si la valeur l'est
+    mode = "0440";
   };
 
   services.newt = {
@@ -19,17 +15,21 @@
     # settings = {
     #   endpoint = "https://pangolin.vps.marcpartensky.com";
     #   id = "4y2rxynfoiseqon";
-    #   # secret = config.sops.secrets.newt_env.value;
     # };
   };
 
-  # systemd.services.newt = {
-  #   enable = true;
-  #   wantedBy = ["multi-user.target"];
-  #   after = ["network-online.target"];
-  #   serviceConfig = {
-  #     ExecStart = "${pkgs.fosrl-newt}/bin/newt --id test --secret i9q2wx1heguo1ix823r9hwm51bnv30d30v7b2plx5pvsehb3 --endpoint https://pangolin.vps.marcpartensky.com";
-  #     Restart = "always";
-  #   };
+  # systemd.services.newt.serviceConfig = {
+  #   DynamicUser = lib.mkForce false;
+  #   PrivateUsers = lib.mkForce false;
+  #   ProtectHome = lib.mkForce false;
+  #   ExecStart = lib.mkForce (pkgs.writeShellScript "newt-start" ''
+  #     set -a
+  #     source ${config.sops.secrets.newt_env.path}
+  #     set +a
+  #     exec ${pkgs.fosrl-newt}/bin/newt \
+  #       -endpoint="$PANGOLIN_ENDPOINT" \
+  #       -id="$NEWT_ID" \
+  #       -secret="$NEWT_SECRET"
+  #   '');
   # };
 }
